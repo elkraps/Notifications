8d06cd167b6888d4d54bec23b339371b933283f4
elkraps <dobroisreal@mail.ru>
2023-07-20 08:40:51 +0300

    Initial Commit

---
+++ Notifications/NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsApp.swift
+//  Notifications
+//
+//  Created by Vlad Dzirko on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
